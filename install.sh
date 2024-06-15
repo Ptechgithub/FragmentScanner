51750be2c7614e37710a60080a0b5b8f070426c6
Peyman <118217228+Ptechgithub@users.noreply.github.com>
2024-06-15 19:10:41 +0330

    update install.sh

--- install.sh
+++ install.sh
@@ -291,156 +291,156 @@ config2Fragment() {
 		if [ "$tls" == "tls" ]; then
 		    # Create the JSON config with TLS
 		    json=$(cat <<EOF
-	{
-	  "remarks": "$name+TLS+Fragment",
-	  "log": {
-	    "access": "",
-	    "error": "",
-	    "loglevel": "warning"
-	  },
-	  "inbounds": [
-	    {
-	      "tag": "socks",
-	      "port": 10808,
-	      "listen": "127.0.0.1",
-	      "protocol": "socks",
-	      "sniffing": {
-	        "enabled": true,
-	        "destOverride": [
-	          "http",
-	          "tls"
-	        ],
-	        "routeOnly": false
-	      },
-	      "settings": {
-	        "auth": "noauth",
-	        "udp": true,
-	        "allowTransparent": false
-	      }
-	    },
-	    {
-	      "tag": "http",
-	      "port": 10809,
-	      "listen": "127.0.0.1",
-	      "protocol": "http",
-	      "sniffing": {
-	        "enabled": true,
-	        "destOverride": [
-	          "http",
-	          "tls"
-	        ],
-	        "routeOnly": false
-	      },
-	      "settings": {
-	        "auth": "noauth",
-	        "udp": true,
-	        "allowTransparent": false
-	      }
-	    }
-	  ],
-	  "outbounds": [
-	    {
-	      "tag": "proxy",
-	      "protocol": "vmess",
-	      "settings": {
-	        "vnext": [
-	          {
-	            "address": "$address",
-	            "port": $port,
-	            "users": [
-	              {
-	                "id": "$uuid",
-	                "alterId": 0,
-	                "email": "email",
-	                "security": "$security",
-	                "encryption": "none",
-	                "flow": ""
-	              }
-	            ]
-	          }
-	        ]
-	      },
-	      "streamSettings": {
-	        "network": "$network",
-	        "security": "tls",
-	        "tlsSettings": {
-	          "allowInsecure": false,
-	          "serverName": "$sni",
-	          "alpn": [
-	            $alpn
-	          ],
-	          "fingerprint": "$fp",
-	          "show": false
-	        },
-	        "wsSettings": {
-	          "path": "$path",
-	          "headers": {
-	            "Host": "$host"
-	          }
-	        },
-	        "sockopt": {
-	          "dialerProxy": "fragment",
-	          "tcpKeepAliveIdle": 100,
-	          "mark": 255,
-	          "tcpNoDelay": true
-	        }
-	      }
-	    },
-	    {
-	      "tag": "fragment",
-	      "protocol": "freedom",
-	      "settings": {
-	        "domainStrategy": "AsIs",
-	        "fragment": {
-	          "packets": "tlshello",
-	          "length": "10-20",
-	          "interval": "10-20"
-	        }
-	      },
-	      "streamSettings": {
-	        "sockopt": {
-	          "tcpNoDelay": true,
-	          "tcpKeepAliveIdle": 100
-	        }
-	      }
-	    },
-	    {
-	      "tag": "direct",
-	      "protocol": "freedom",
-	      "settings": {}
-	    },
-	    {
-	      "tag": "block",
-	      "protocol": "blackhole",
-	      "settings": {
-	        "response": {
-	          "type": "http"
-	        }
-	      }
-	    }
-	  ],
-	  "routing": {
-	    "domainStrategy": "AsIs",
-	    "rules": [
-	      {
-	        "type": "field",
-	        "inboundTag": [
-	          "api"
-	        ],
-	        "outboundTag": "api",
-	        "enabled": true
-	      },
-	      {
-	        "id": "5627785659655799759",
-	        "type": "field",
-	        "port": "0-65535",
-	        "outboundTag": "proxy",
-	        "enabled": true
-	      }
-	    ]
-	  }
-	}
-	EOF
+{
+  "remarks": "$name+TLS+Fragment",
+  "log": {
+    "access": "",
+    "error": "",
+    "loglevel": "warning"
+  },
+  "inbounds": [
+    {
+      "tag": "socks",
+      "port": 10808,
+      "listen": "127.0.0.1",
+      "protocol": "socks",
+      "sniffing": {
+        "enabled": true,
+        "destOverride": [
+          "http",
+          "tls"
+        ],
+        "routeOnly": false
+      },
+      "settings": {
+        "auth": "noauth",
+        "udp": true,
+        "allowTransparent": false
+      }
+    },
+    {
+      "tag": "http",
+      "port": 10809,
+      "listen": "127.0.0.1",
+      "protocol": "http",
+      "sniffing": {
+        "enabled": true,
+        "destOverride": [
+          "http",
+          "tls"
+        ],
+        "routeOnly": false
+      },
+      "settings": {
+        "auth": "noauth",
+        "udp": true,
+        "allowTransparent": false
+      }
+    }
+  ],
+  "outbounds": [
+    {
+      "tag": "proxy",
+      "protocol": "vmess",
+      "settings": {
+        "vnext": [
+          {
+            "address": "$address",
+            "port": $port,
+            "users": [
+              {
+                "id": "$uuid",
+                "alterId": 0,
+                "email": "email",
+                "security": "$security",
+                "encryption": "none",
+                "flow": ""
+              }
+            ]
+          }
+        ]
+      },
+      "streamSettings": {
+        "network": "$network",
+        "security": "tls",
+        "tlsSettings": {
+          "allowInsecure": false,
+          "serverName": "$sni",
+          "alpn": [
+            $alpn
+          ],
+          "fingerprint": "$fp",
+          "show": false
+        },
+        "wsSettings": {
+          "path": "$path",
+          "headers": {
+            "Host": "$host"
+          }
+        },
+        "sockopt": {
+          "dialerProxy": "fragment",
+          "tcpKeepAliveIdle": 100,
+          "mark": 255,
+          "tcpNoDelay": true
+        }
+      }
+    },
+    {
+      "tag": "fragment",
+      "protocol": "freedom",
+      "settings": {
+        "domainStrategy": "AsIs",
+        "fragment": {
+          "packets": "tlshello",
+          "length": "10-20",
+          "interval": "10-20"
+        }
+      },
+      "streamSettings": {
+        "sockopt": {
+          "tcpNoDelay": true,
+          "tcpKeepAliveIdle": 100
+        }
+      }
+    },
+    {
+      "tag": "direct",
+      "protocol": "freedom",
+      "settings": {}
+    },
+    {
+      "tag": "block",
+      "protocol": "blackhole",
+      "settings": {
+        "response": {
+          "type": "http"
+        }
+      }
+    }
+  ],
+  "routing": {
+    "domainStrategy": "AsIs",
+    "rules": [
+      {
+        "type": "field",
+        "inboundTag": [
+          "api"
+        ],
+        "outboundTag": "api",
+        "enabled": true
+      },
+      {
+        "id": "5627785659655799759",
+        "type": "field",
+        "port": "0-65535",
+        "outboundTag": "proxy",
+        "enabled": true
+      }
+    ]
+  }
+}
+EOF
 	)
 	    else
 	        # VMESS NO TLS
